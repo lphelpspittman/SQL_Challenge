54228d4c586a581111a15b1087507db8e1d3ca44
Lauren Phelps <laphleps08@outlook.com>
2023-05-15 18:08:49 -0400

    updated schema file

--- schema_for_PH_Employee_db.sql
+++ schema_for_PH_Employee_db.sql
@@ -34,8 +34,71 @@ CREATE TABLE employees (
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	sex VARCHAR,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
 	--primary key
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
+
+-- use import function in pgAdmin to load csv file into the employees table
+-- view employees table
+SELECT *
+FROM employees;
+
+-- create departments table
+-- "dept_no","dept_name"
+CREATE TABLE departments(
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	--identify primary key
+	PRIMARY KEY(dept_no)
+);
+
+-- view departments
+SELECT *
+FROM departments;
+
+-- create dept_manager table
+--dept_no,emp_no
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL,
+	-- need a unique key for our primary key
+	PRIMARY KEY (dept_no, emp_no),
+	-- link this table to employees table and to departments table
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- view dept_manager
+SELECT*
+FROM dept_manager
+
+-- create dept_emp table
+-- emp_no,dept_no
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	-- link this table to employees table and to departments table
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- view table
+SELECT *
+FROM dept_emp;
+
+-- create salaries table
+-- emp_no, salary
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no, salary),
+	--relate to employee table
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- VIEW TABLE
+SELECT *
+FROM salaries;
